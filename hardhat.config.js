5128603bd4006822771b7970307684af60db1ae4
Christopher Flattum <cflattum@usc.edu>
2022-04-27 22:32:13 -0700

    added ropsten to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,10 +12,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
-
+const ALCHEMY_API_KEY = "k7a2O4LX-uqidThLlerNHdn1aESFNvca";
+const ROPSTEN_PRIVATE_KEY = "730d78b7b823cc687e8e432bf766715639690f5cb728f8bd24387c2e7d270fb2";
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    ropsten:{
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
